26f70b8aba113062409edc1d0ab7f729920f6a6d
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2022-11-14 12:40:21 +0100

    Complete 82 first part (Controller, SimpleGUI)

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String SEPARATOR = File.separator;  
+    private static final String PATH = System.getProperty("user.home") + SEPARATOR + "output.txt";
+    private File currFile;
+
+    public Controller() {
+        this.currFile = new File(PATH);
+    }
+
+    public void setCurrentFile(final File file) {
+        this.currFile = file;
+    }
+
+    public File getFile() {
+        return this.currFile;
+    }
+
+    public String getPath() {
+        return this.currFile.getPath();
+    }
+
+    public void writeString(final String string) throws IOException{
+        try (final PrintStream ps = new PrintStream(currFile, StandardCharsets.UTF_8)) {
+            ps.print(string);
+        } catch (final IOException e) {
+            throw new IOException("Error while writing string", e);
+        }
+    }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,16 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +19,55 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+
+    public SimpleGUI(final Controller controller) {
+        final JPanel pan = new JPanel(new BorderLayout());
+        this.frame.getContentPane().add(pan);
+        final JTextArea textArea = new JTextArea();
+        pan.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        pan.add(save, BorderLayout.SOUTH);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                final String text = textArea.getText();
+                try {
+                    controller.writeString(text);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUI(new Controller()).display();
+    }
 
 }
